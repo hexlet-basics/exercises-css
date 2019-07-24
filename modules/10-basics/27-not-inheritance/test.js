62acb4594bfbff3324abf70015c42625bb105002
n.mikhaylov <nmikhaylov92@gmail.com>
2019-07-24 12:28:58 +0300

    union 25-inheritance and 27-not-inheritance lessons

--- modules/10-basics/27-not-inheritance/test.js
+++
@@ -1,8 +0,0 @@
-const { test } = require('tests');
-
-test(({ query, expect }) => {
-  const p = query(document, 'p');
-
-  const styleP = getComputedStyle(p);
-  expect(styleP).to.have.property('border', '5px');
-});

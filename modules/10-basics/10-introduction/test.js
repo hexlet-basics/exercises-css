328ba41a0538a840f412e16b8cc7df284b7d2085
Kirill Mokevnin <mokevnin@gmail.com>
2019-07-24 14:53:21 -0400

    add tests

---
+++ modules/10-basics/10-introduction/test.js
@@ -0,0 +1,8 @@
+const { test } = require('tests');
+
+test(({ query, expect }) => {
+  const element = query(document, 'p');
+
+  const style = getComputedStyle(element);
+  expect(style).to.have.property('font-size', '12px');
+});

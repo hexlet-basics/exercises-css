8560cc15fe80929df71fe6d833ccd08dfb353093
n.mikhaylov <nmikhaylov92@gmail.com>
2019-07-26 13:26:39 +0300

    add 70-font-family lesson

---
+++ modules/20-text/70-font-family/test.js
@@ -0,0 +1,9 @@
+const { test } = require('tests');
+
+test(({ query, expect }) => {
+  const element = query(document, 'p');
+  expect(element).to.have.class('verdana-text');
+
+  const style = getComputedStyle(element);
+  expect(style).to.have.property('font-family', 'Verdana');
+});

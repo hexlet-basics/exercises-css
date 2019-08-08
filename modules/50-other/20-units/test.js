69a6c9d706416ceff239532aaf1463b70fe89029
n.mikhaylov <nmikhaylov92@gmail.com>
2019-08-08 12:57:38 +0300

    add test in 20-units lesson

--- modules/50-other/20-units/test.js
+++ modules/50-other/20-units/test.js
@@ -1,5 +1,12 @@
 const { test } = require('tests');
 
 test(({ query, expect }) => {
-  expect(true);
+  const div = query(document, '.header');
+  const p = query(div, '.offer');
+
+  const divStyle = getComputedStyle(div);
+  const pStyle = getComputedStyle(p);
+
+  expect(divStyle).to.have.property('font-size', '22px');
+  expect(pStyle).to.have.property('font-size', '200%');
 });

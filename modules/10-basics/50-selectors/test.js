8e95937621ff99580c8b74913b9a0db134a5ac2f
n.mikhaylov <nmikhaylov92@gmail.com>
2020-08-05 17:26:54 +0300

    add 50-selectors lesson

---
+++ modules/10-basics/50-selectors/test.js
@@ -0,0 +1,10 @@
+const { test } = require('tests');
+
+test(({ query, expect }) => {
+  const element = query(document, 'p');
+  expect(element).to.have.class('bold');
+  expect(element).to.have.id('red');
+  const style = getComputedStyle(element);
+  expect(style).to.have.property('font-weight', 'bold');
+  expect(style).to.have.property('color', 'red');
+});

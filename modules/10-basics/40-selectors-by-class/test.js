28f446d4cfb2ee5fbf0aaeafc29ecb7bef8875e0
zakharoffam <zakharoff.am@ya.ru>
2021-12-16 21:02:15 +0400

    Исправлен тест для соответствия заданию 10-40

--- modules/10-basics/40-selectors-by-class/test.js
+++ modules/10-basics/40-selectors-by-class/test.js
@@ -4,5 +4,5 @@ test(({ query, expect }) => {
   const element = query(document, 'p');
   expect(element).to.have.class('news');
   const style = getComputedStyle(element);
-  expect(style).to.have.property('color', '#333333');
+  expect(style).to.have.property('color', '#553333');
 });
